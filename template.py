b15cc4df107828422f35ae23433cac04b243b52c
SagruH <sagru2899@googlemail.com>
2020-08-03 13:26:09 +0200

    template update

--- template.py
+++ template.py
@@ -9,14 +9,32 @@ from scipy import constants as const
 #from kafe.function_tools import FitFunction, LaTeX, ASCII
 #from kafe.function_library import quadratic_3par
 
-#import uncertainties as uc
-#from uncertainties.umath import sqrt
+import uncertainties as uc
+from uncertainties.umath import sqrt
 
 
-def loadCSV(name,hlines=1,split=2):
+def loadCSV(name,hlines=1,split=2): #liest eine , getrennte CSV ein und teilt in arrays nach spalten
     hlines, data = ppk.readCSV(name,hlines)
     data = np.array(data)
     a,b=np.split(data,split)
     a = a[0] # anpassen nach split
     b = b[0]
     return a,b;
+
+def plot(data):  # alle zeilen zum ploten von daten
+    plt.plot(data[0],data[1],label="test")
+    plt.legend()
+    plt.title("Messdaten")
+    plt.xlabel("h in cm")
+    plt.ylabel("t in s")
+    plt.grid(True)
+    plt.show()
+    return;
+
+def lineareRegression(x,y):
+    return slope, intercept, r_value, p_value, std_err = stats.linregress(x, y);
+
+'''
+fehler rechung mit uncertainties
+variable = uc.float(wert, fehler)
+'''
