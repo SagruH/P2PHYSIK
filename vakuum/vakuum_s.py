8f5b29a30a5e8352918edf50ee1110d8194c01a4
SagruH <sagru2899@googlemail.com>
2020-08-05 14:41:22 +0200

    Vakuum Aufgabe 2 final

--- vakuum/vakuum_s.py
+++ vakuum/vakuum_s.py
@@ -24,10 +24,37 @@ def loadCSV(name,hlines=1,split=3): #liest eine , getrennte CSV ein und teilt in
 
 def aufgabe2():
     t,p1,p2 = loadCSV("2aDaten.csv")
-    print(t,p1,p2)
+    p0 = 1000 #mbar
+    V = 10.1 #l
+    l = 0.51 #m
+    d = 0.002 #m
+    xw = np.linspace(0,420,1000)
 
+    p1ln = np.log(p1/p0)
+    p2ln = np.log(p2/p0)
+    p1n = np.array(p1ln[1:15])
+    p2n = np.array(p2ln[15:23])
+    t1n = np.array(t[1:15])
+    t2n = np.array(t[15:23])
 
+    slope1, intercept1, r_value, p_value, std_err = stats.linregress(t1n, p1n)
+    slope2, intercept2, r_value, p_value, std_err = stats.linregress(t2n, p2n)
+    S1 = -V*slope1
+    S2 = -V*slope2
+    L = 1/ ( (1/S2) - (1/S1) )
+    print("S1 = ",S1)
+    print("S2 = ",S2)
+    print("L = ",L)
 
+    plt.plot(t,p1ln,"or",label="P1")
+    plt.plot(t1n,slope1*t1n+intercept1,"-b")
+    plt.plot(t,p2ln,"og",label="P2")
+    plt.plot(t2n,slope2*t2n+intercept2,"-c")
+    plt.legend()
+    plt.xlabel("t in s")
+    plt.ylabel("-ln(p/p0)")
+    plt.grid(True)
+    plt.show()
 
     return;
 
