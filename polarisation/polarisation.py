653dfe5f1a2f79db95f71771e9a5a8eaf2d14a72
SagruH <sagru2899@googlemail.com>
2020-08-07 11:54:23 +0200

    Polarisation plots

--- polarisation/polarisation.py
+++ polarisation/polarisation.py
@@ -5,10 +5,39 @@ import matplotlib.pyplot as plt
 from scipy import stats
 from scipy import constants as const
 
-def loadCSV(name,hlines=1,split=2): #liest eine , getrennte CSV ein und teilt in arrays nach spalten
+def loadCSV(name,hlines=2,split=2): #liest eine , getrennte CSV ein und teilt in arrays nach spalten
     hlines, data = ppk.readCSV(name,hlines)
     data = np.array(data)
     a,b=np.split(data,split)
     a = a[0] # anpassen nach split
     b = b[0]
     return a,b;
+
+def polplots(phi,r):
+    phi2 = phi+180
+    phi = np.hstack((phi,phi2))
+    r = np.hstack((r,r))
+    plt.polar((phi/360)*2*np.pi,r,"ob")
+    #plt.xlabel("h in cm")
+    #plt.ylabel("t in s")
+    plt.grid(True)
+    plt.show()
+
+    return;
+
+def main():
+    almFF, ulmFF = loadCSV("datenlinmFF.csv")
+    aloFF, uloFF = loadCSV("datenlinoFF.csv")
+    acir, ucir = loadCSV("datenCir.csv")
+    ae50, ue50 = loadCSV("datenelip50.csv")
+    ae60, ue60 = loadCSV("datenelip60.csv")
+
+    angel = np.array([almFF,aloFF,acir,ae50,ae60])
+    volt = np.array([ulmFF,uloFF,ucir,ue50,ue60])
+    polplots(angel[0],volt[0])
+
+
+
+    return;
+
+main()
