8c168bb4d7d52c911298af5329547131e52436e3
SagruH <sagru2899@googlemail.com>
2020-08-12 12:01:32 +0200

    franckhertz code ini

---
+++ franckhertz/fhcode.py
@@ -0,0 +1,13 @@
+import numpy as np
+import PhyPraKit as ppk #wurde von mir verändert
+import matplotlib.pyplot as plt
+
+from scipy import stats
+from scipy import constants as const
+
+#import kafe
+#from kafe.function_tools import FitFunction, LaTeX, ASCII
+#from kafe.function_library import quadratic_3par
+
+import uncertainties as uc
+from uncertainties.umath import sqrt
